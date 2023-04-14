7012e58e632374438edb81d9c22809a60d52ade7
laazar26 <lazardjordjevicofficial2@gmail.com>
2023-04-14 13:55:21 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e814535a-9cc0-46b5-a221-d89f20ee4d62';
+export const MODAL_CLOSE_SEC = 2.5;
